d785dab6bdeed3b5deee9fcd9e3d4c8ab481797c
Joe Newman <jj_newman@hotmail.com>
2015-04-22 18:20:19 +0100

    Update game.rb

--- lib/game.rb
+++ lib/game.rb
@@ -11,24 +11,21 @@ def players
   @players ||= []
 end
 
-# to be refactored!!
 def winner(player1, player2)
   if (player1.choice == :rock && player2.choice == :scissors)
-      player1.name
+    player1.name
   elsif (player2.choice == :rock && player1.choice == :scissors)
-      player2.name
+    player2.name
   elsif (player1.choice == :paper && player2.choice == :rock)
-      player1.name
+    player1.name
   elsif (player2.choice == :paper && player1.choice == :rock)
-      player2.name
+    player2.name
   elsif (player1.choice == :scissors && player2.choice == :paper)    
-      player1.name
+    player1.name
   elsif (player2.choice == :scissors && player1.choice == :paper)    
-      player2.name
+    player2.name
   else
-      :draw
+    :draw
   end
 end
-
-
 end
